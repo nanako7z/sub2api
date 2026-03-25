d27397b194b49267771e21c6a2ec7dbfbb8767b0
luli <luli.dai@aloudata.com>
2026-03-25 20:08:25 +0800

    chore(mitm): force lowercase proxy env and clear no_proxy in wrapper

--- tools/mitm/claude_with_proxy.sh
+++ tools/mitm/claude_with_proxy.sh
@@ -11,6 +11,11 @@ fi
 export HTTPS_PROXY="http://127.0.0.1:${PORT}"
 export HTTP_PROXY="http://127.0.0.1:${PORT}"
 export ALL_PROXY="http://127.0.0.1:${PORT}"
+export https_proxy="$HTTPS_PROXY"
+export http_proxy="$HTTP_PROXY"
+export all_proxy="$ALL_PROXY"
+unset NO_PROXY
+unset no_proxy
 export NODE_EXTRA_CA_CERTS="$CA_CERT"
 
 exec "$@"
